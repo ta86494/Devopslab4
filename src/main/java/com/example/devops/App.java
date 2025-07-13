1ccfa532c18743e9e50fadf7d6eccfbf47c69e87
ta86494 <2021wa86494@wilp.bits-pilani.ac.in>
2025-07-13 22:17:44 +0530

    updated jenkins CI

--- src/main/java/com/example/devops/App.java
+++ src/main/java/com/example/devops/App.java
@@ -1,13 +1,13 @@
 package com.example.devops;
 
 /**
- * Hello world!
+ * Hello jenkins!
  *
  */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello jenkins CICD" );
     }
 }
